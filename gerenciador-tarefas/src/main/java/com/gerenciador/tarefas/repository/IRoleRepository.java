6432652844341401b824df527258dbd365527f48
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2024-01-24 14:41:03 -0300

    feat: setting user roles

--- gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/repository/IRoleRepository.java
+++ gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/repository/IRoleRepository.java
@@ -1,6 +1,7 @@
 package com.gerenciador.tarefas.repository;
 
 import com.gerenciador.tarefas.entity.eRole;
+import com.gerenciador.tarefas.permission.PermissionEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface IRoleRepository extends JpaRepository<eRole, Long> {
 
-    Optional<eRole> findByName(String username);
+    eRole findByName(PermissionEnum name);
 }
--- gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/service/UserService.java
+++ gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/service/UserService.java
@@ -1,7 +1,10 @@
 package com.gerenciador.tarefas.service;
 
+import com.gerenciador.tarefas.entity.eRole;
 import com.gerenciador.tarefas.entity.eUser;
+import com.gerenciador.tarefas.repository.IRoleRepository;
 import com.gerenciador.tarefas.repository.IUserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -9,20 +12,41 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserService {
 
     @Autowired
     private IUserRepository iUserRepository;
 
     @Autowired
+    private IRoleRepository iRoleRepository;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     public eUser saveUser(eUser user){
+
+        //Recuperando as roles existentes do banco
+        List<eRole> userRoles = user.getRoles()
+                            .stream()
+                            .map(role -> iRoleRepository.findByName(role.getName())).toList();
+
+        user.setRoles(userRoles);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return this.iUserRepository.save(user);
     }
 
     public eUser updateUser(eUser user){
+
+        //Recuperando as roles existentes do banco
+        List<eRole> userRoles = user.getRoles()
+                .stream()
+                .map(role -> iRoleRepository.findByName(role.getName())).toList();
+
+        user.setRoles(userRoles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return this.iUserRepository.save(user);
     }
 
