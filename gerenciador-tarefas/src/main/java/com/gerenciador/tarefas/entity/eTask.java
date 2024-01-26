78b3101853a865cf1ed93affda53ea3f7c5deb14
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2024-01-26 20:33:42 -0300

    fix: eTask

--- gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/entity/eTask.java
+++ gerenciador-tarefas/src/main/java/com/gerenciador/tarefas/entity/eTask.java
@@ -2,10 +2,7 @@ package com.gerenciador.tarefas.entity;
 
 import com.gerenciador.tarefas.status.TaskStatusEnum;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -17,6 +14,8 @@ import java.time.LocalTime;
 @Data
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class eTask implements Serializable {
 
